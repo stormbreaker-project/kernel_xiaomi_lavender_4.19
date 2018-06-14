10757877ca055c9f743f69e6a5d7c763f3c8fa43
Vijay kumar Tumati <vtumati@codeaurora.org>
2018-06-14 09:27:24 +0530

    msm: camera: sensor: Add support for front aux sensor
    Allow front aux sensor to be connected on device.
    
    Change-Id: I0386c23c77b38200c20581cd85b20c96bf074547
    Signed-off-by: Vijay kumar Tumati <vtumati@codeaurora.org>

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -54,6 +54,8 @@
 
 #define SECURE_CAM_RST_MODULES
 
+#define FRONT_AUX_SENSOR_SUPPORT
+
 enum msm_sensor_camera_id_t {
 	CAMERA_0,
 	CAMERA_1,
@@ -74,6 +76,7 @@ enum camb_position_t {
 	BACK_CAMERA_B,
 	FRONT_CAMERA_B,
 	AUX_CAMERA_B = 0x100,
+	FRONT_AUX_CAMERA_B,
 	INVALID_CAMERA_B,
 };
 
