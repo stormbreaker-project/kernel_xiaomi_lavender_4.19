7504b87b4f010e405efc875f378b96164f5bd3d9
Pavel Dubrova <pashadubrova@gmail.com>
2019-11-29 13:05:51 +0200

    trace: Add Kconfig option for trace_printk()
    Trace_printk() was used to log debug messages where printk()
    isn't feasible. As there should not be a single caller to
    trace_printk() in production kernels, replace them with empty
    functions.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -2833,6 +2833,7 @@ static void put_trace_buf(void)
 	this_cpu_dec(trace_percpu_buffer->nesting);
 }
 
+#ifdef CONFIG_TRACE_PRINTK
 static int alloc_percpu_trace_buffer(void)
 {
 	struct trace_buffer_struct *buffers;
@@ -2844,11 +2845,13 @@ static int alloc_percpu_trace_buffer(void)
 	trace_percpu_buffer = buffers;
 	return 0;
 }
+#endif /* CONFIG_TRACE_PRINTK */
 
 static int buffers_allocated;
 
 void trace_printk_init_buffers(void)
 {
+#ifdef CONFIG_TRACE_PRINTK
 	if (buffers_allocated)
 		return;
 
@@ -2885,6 +2888,9 @@ void trace_printk_init_buffers(void)
 	 */
 	if (global_trace.trace_buffer.buffer)
 		tracing_start_cmdline_record();
+#else
+	return;
+#endif /* CONFIG_TRACE_PRINTK */
 }
 
 void trace_printk_start_comm(void)
