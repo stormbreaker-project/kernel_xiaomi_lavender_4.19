6db0cb82db0257de2da8028ec0d05f40eafd2aed
LibXZR <xzr467706992@163.com>
2020-09-07 22:30:18 +0800

    cpu_input_boost: Expose dynamic sched_boost node
    * Useful for users who want to tune it manully
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -28,6 +28,8 @@ static unsigned short input_boost_duration __read_mostly =
 	CONFIG_INPUT_BOOST_DURATION_MS;
 static unsigned short wake_boost_duration __read_mostly =
 	CONFIG_WAKE_BOOST_DURATION_MS;
+	
+static bool dynamic_sched_boost __read_mostly = true;
 
 module_param(input_boost_freq_lp, uint, 0644);
 module_param(input_boost_freq_hp, uint, 0644);
@@ -36,6 +38,8 @@ module_param(max_boost_freq_hp, uint, 0644);
 
 module_param(input_boost_duration, short, 0644);
 module_param(wake_boost_duration, short, 0644);
+
+module_param(dynamic_sched_boost, bool, 0644);
 
 enum {
 	SCREEN_OFF,
@@ -107,7 +111,8 @@ static void __cpu_input_boost_kick(struct boost_drv *b)
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
-	sched_set_boost(2);
+	if (dynamic_sched_boost)
+		sched_set_boost(2);
 	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
 			      msecs_to_jiffies(input_boost_duration)))
 		wake_up(&b->boost_waitq);
@@ -160,7 +165,8 @@ static void input_unboost_worker(struct work_struct *work)
 					   typeof(*b), input_unboost);
 
 	clear_bit(INPUT_BOOST, &b->state);
-	sched_set_boost(0);
+	if (dynamic_sched_boost)
+		sched_set_boost(0);
 	wake_up(&b->boost_waitq);
 
 	sysctl_sched_energy_aware = 1;
@@ -301,7 +307,8 @@ free_handle:
 
 static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 {
-	sched_set_boost(0);
+	if (dynamic_sched_boost)
+		sched_set_boost(0);
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
